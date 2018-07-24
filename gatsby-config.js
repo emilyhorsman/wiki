d2bf47eda9472edb57c4b4a1426f73ca4245fe2a
Emily Horsman <me@emilyhorsman.com>
2018-07-23 22:26:38 -0700

    Add mdx support

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: { name: 'pages', path: `${__dirname}/src/mdx` },
+    },
+  ],
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,52 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+const mdxTestRe = /\.mdx?$/;
+
+const mdxQuery = graphql =>
+  graphql(`
+    {
+      allFile(filter: { extension: { regex: "/^mdx?$/" } }) {
+        edges {
+          node {
+            absolutePath
+            relativeDirectory
+            name
+          }
+        }
+      }
+    }
+  `);
+
+exports.onCreateWebpackConfig = ({ actions, loaders }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: mdxTestRe,
+          use: [loaders.js(), '@mdx-js/loader'],
+        },
+      ],
+    },
+  });
+};
+
+exports.createPages = ({ graphql, actions: { createPage } }) => {
+  return new Promise((resolve, reject) => {
+    const query = mdxQuery(graphql)
+      .then(result => {
+        if (result.errors) {
+          return reject(result.errors);
+        }
+
+        result.data.allFile.edges
+          .map(edge => edge.node)
+          .map(({ absolutePath, relativeDirectory, name }) => ({
+            path: `${relativeDirectory}/${name}`,
+            component: absolutePath,
+          }))
+          .forEach(page => createPage(page));
+      })
+      .then(resolve);
+  });
+};
